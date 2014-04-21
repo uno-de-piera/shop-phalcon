b22a8fb17d2dc6716595bb26f73e0a57cfccfe11
Israel Parra <unodepiera@uno-de-piera.com>
2014-04-21 12:51:37 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -15,6 +15,6 @@ return new \Phalcon\Config(array(
 		'pluginsDir'     => __DIR__ . '/../../app/plugins/',
 		'libraryDir'     => __DIR__ . '/../../app/library/',
 		'cacheDir'       => __DIR__ . '/../../app/cache/',
-		'baseUri'        => '/shopPhalcon/',
+		'baseUri'        => '/shop-phalcon/',
 	)
 ));
